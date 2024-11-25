ce931a34ba3e2bf0a94cea195c6e8627b7385c57
PetridhsG <giannispetridakos@gmail.com>
2024-11-25 21:55:40 +0200

    Exercise 5 almost finished (pictures in exercise 4 and also invalid input with css)

---
+++ Exercise5/js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Rule 1: Check if passwords match
+    const password = document.getElementById("password");
+    const confirm_password = document.getElementById("confirm_password");
+    confirm_password.addEventListener("input" , () => { 
+        confirm_password.setCustomValidity(
+            confirm_password === password ? "" : "Passwords don't match."
+        );  
+    })
+
+    // Rule 2: Restrict minimum age to 18
+    const birthDateElement = document.getElementById("birthdate");
+    birthDateElement.addEventListener("input", () => {
+        const today = new Date();
+        const birthDate = new Date(birthDateElement.value);
+        const age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth(); 
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        birthDateElement.setCustomValidity(
+            age >= 18 ? "" : "You must be at least 18 years old to register."
+        )   
+    });
+
+    // Rule 3: Username must start with a letter,be at least 5 characters and contains only letters and numbers
+    const username = document.getElementById("username");
+    username.addEventListener("input", () =>{  
+        const pattern = /^[a-zA-Z][a-zA-Z0-9]{4,}$/;
+        username.setCustomValidity(
+            pattern.test(username.value) ? "" : "Username must start with a letter,be at least 5 characters and contain only number and letters."
+        );
+    })
+}
+);
